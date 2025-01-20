b0221312e1142f50632a78fce131b0c29627eb0f
Khadija2504 <khadijalady25@gmail.com>
2025-01-20 20:54:49 +0100

    PC-26 add the season componet with its form and style to add seasons as an organizer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m=> m.AuthModule)},
   {path: 'pigeon', loadChildren: () => import('./pigeon/pigeon.module').then(m => m.PigeonModule), canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'breeder' }},
   {path: 'error', loadChildren: () => import('./error/error.module').then(m=> m.ErrorModule)},
+  {path: 'season', loadChildren: () => import('./season/season.module').then(m=> m.SeasonModule), canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'organizer' }},
   {path: '', redirectTo: '/auth', pathMatch: 'full'},
 ];
 
