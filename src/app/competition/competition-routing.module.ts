2bebfe895e678204d6ddfdea4020f11a74516af6
Khadija2504 <khadijalady25@gmail.com>
2025-01-21 15:02:21 +0100

    PC-15 assign the pigeons to the competitions, add its route and nav link with its service

--- src/app/competition/competition-routing.module.ts
+++ src/app/competition/competition-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCompetitionComponent } from './create-competition/create-competition.component';
+import { AssignCompetitionComponent } from './assign-competition/assign-competition.component';
 
 const routes: Routes = [
   {path: 'create-competition', component: CreateCompetitionComponent},
+  {path: 'assign-pigeon', component: AssignCompetitionComponent},
 ];
 
 @NgModule({
