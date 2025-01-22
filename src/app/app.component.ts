1cc6907991142fae1cd4db417d912b17d46d3584
Khadija2504 <khadijalady25@gmail.com>
2025-01-22 16:42:47 +0100

    PC-22 update the auth service and app component by adding logout inside it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,27 @@ export class AppComponent implements OnInit {
   isLoggedIn: boolean = false;
   userRole: string | null = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router:Router) {}
 
   ngOnInit(): void {
+    this.checkLoginStatus();
     this.isLoggedIn = this.authService.isLoggedIn();
     this.userRole = this.authService.getUserRole();
+  }
+
+  checkLoginStatus(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
   }
 
   hasRole(role: string): boolean {
     console.log(role);
     console.log(this.authService.getUserRole());
-    
     return this.authService.getUserRole() === role;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.checkLoginStatus();
+    setTimeout(()=> this.router.navigate(['/auth/login']));
   }
 }
