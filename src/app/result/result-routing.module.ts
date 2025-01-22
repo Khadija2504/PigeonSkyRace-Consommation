b0ac8f4300bcb05de5175b36261d9ae70810b9c0
Khadija2504 <khadijalady25@gmail.com>
2025-01-22 12:30:36 +0100

    PC-22 add the upload data results component for the upload filr to the backend, add its route with service function

--- src/app/result/result-routing.module.ts
+++ src/app/result/result-routing.module.ts
@@ -4,10 +4,13 @@ import { DisplayResultsComponent } from './display-results/display-results.compo
 import { AuthGuard } from '../auth/auth.guard';
 import { RoleGuard } from '../auth/role.guard';
 import { DownloadResultsComponent } from './download-results/download-results.component';
+import { UploadArrivalDataComponent } from './upload-arrival-data/upload-arrival-data.component';
 
 const routes: Routes = [
   {path: 'display-results', component: DisplayResultsComponent, canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'breeder' }},
-  {path: 'download-results', component: DownloadResultsComponent, canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'breeder' }}
+  {path: 'download-results', component: DownloadResultsComponent, canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'breeder' }},
+  {path: 'upload-arrival-data', component: UploadArrivalDataComponent, canActivate: [AuthGuard, RoleGuard], data: { expectedRole: 'organizer' }}
+  
 ];
 
 @NgModule({
